59463eb9fd14844a9716f5717b197d2c056cd1f3
fayco226 <faicallengane55@gmail.com>
2022-12-30 12:55:47 +0000

    save

--- Exercice_XP-day-3/solution.py
+++ Exercice_XP-day-3/solution.py
@@ -74,3 +74,34 @@ brand = {
     "US": ["pink", "green"]
     }
 }
+
+brand["number_stores"] = 2
+print("Zaras clients are:")
+for i in brand["type_of_clothes"]:
+    print(i, end=' ')
+
+brand["country_creation"] = "Spain"
+
+k = "international_competitors"
+ 
+if k in brand:
+    brand[k].append("Desigual")
+
+del brand["creation_date"]
+
+print("the last international competitor are ",brand["international_competitors"][-1])
+print("the major clothes colors in the US are ")
+for i in brand["major_color"]["US"]:
+    print(i, end=' ')
+    
+
+print("the amount of key value pairs are ",len(brand))
+print("\n thekeys of dictionary are ")
+for i in brand.keys():
+    print(i, end=' ')
+    
+more_on_zara = {"creation_date": 1975, "number_stores": 10000}
+
+brand.update(more_on_zara)
+
+print(brand["number_stores"]) # number of store is update to 10 000
